8a1ae347ace2a79787515de8e1ca5fb578639b46
unknown <DFraser@GKTL0642.cds.int>
2014-04-10 14:25:29 -0400

    Cleaned up equation class->needs more testing

--- rampart/cannonball.py
+++ rampart/cannonball.py
@@ -76,9 +76,23 @@ class Bezier():
         self.coeffs = []
 
     def add_point(self,point):
+        '''
+        a function to add a control point to the bezier curve
+        Parameters:
+            point: the point to add (Point)
+        Returns:
+            None
+        '''
         self.points.append(point)
 
     def set_coeffs(self):
+        '''
+        a function that set the coefficients for the blending function
+        Parameters:
+            None
+        Returns:
+            None
+        '''
         n = len(self.points) - 1
         self.coeffs = []
         for k in range(n+1):
@@ -92,9 +106,14 @@ class Bezier():
                 self.coeffs[k] /=j
                 j -= 1
 
-    def get_point(self,u,i,point):
-        
-        
+    def get_point(self,u):
+        '''
+        a function that calculate the bezier point
+        Parameters:
+            u: the value of u for blending function (p1=0<=u<=1.0=pn) (float)
+        Returns:
+            point: an object with the x and  position (Point)
+        '''
         control_points = len(self.points)
         n = control_points - 1
         point = Point()
@@ -107,12 +126,23 @@ class Bezier():
             point.set(x=xi+x*blend_function, y=yi+y*blend_function)
             k += 1
         return point 
-
+    
+    def print_points(self):
+        '''
+        a function that points the bezier points
+        '''
+        print("Bezier Points\n----------------------------")
+        for point in self.points:
+            print(point.get())
+        print("----------------------------")
+    
 class TestBezier(unittest.TestCase):
     def setUp(self):
         self.b = Bezier()
+
     def tearDown(self):
         pass
+
     def test_add_point(self):
         self.assertEqual(self.b.points, [])
         p = Point()
@@ -137,48 +167,120 @@ class TestBezier(unittest.TestCase):
         p3.set(x=3, y=1)
         self.b.coeffs = [1,3,1]
         self.b.points = [p1,p2,p3]
-        u = 0
-        x = 0
-        n = len(self.b.points)
-        p = Point()
-        p.set(x=0,y=0)
-        while x <= n:
-            u = x / float(n)
-            p = self.b.get_point(u,x,p)
-            print(p.get())
-            x += 1
-        
+        p0 = self.b.get_point(0.0)
+        p1 = self.b.get_point(0.25)
+        p2 = self.b.get_point(0.5)
+        p3 = self.b.get_point(0.75)
+        p4 = self.b.get_point(1.0)
+        self.assertEqual(p0.get(),(1.0,1.0))
+        self.assertEqual(p1.get(),(1.875,1.75))
+        self.assertEqual(p2.get(),(2.5,2.0))
+        self.assertEqual(p3.get(),(2.875,1.75))
+        self.assertEqual(p4.get(),(3.0,1.0))
+
 class Equation():
     def __init__(self):
-        self.acceleration = GRAVITY
-        self.velocity = None
-        self.ux = None
-        self.uy = None
+        self.bezier = Bezier()
+        self.u = 0
+        self.step = 0
+        self.tol = 10
+        self.done = False
 
-    def set_equation(self,p1,p2,velocity):
-        #percentage of the shot
-        euclidean = math.sqrt(dx^2+dy^2)
-        self.velocity = velocity
-        vx = (dx/euclidean) * self.velocity
-        vy = (dy/euclidean) * self.velocity
-        if(vx > vy):
-            #more hotizontal, less arc
-            pass
+    def set_equation(self,p1,p2):
+        '''
+        a function that takes two points and creates the bezier equation
+        Parameters:
+            p1: the first point (Point)
+            p2: the second point (Point)
+        Returns:
+            None
+        '''
+        (x1,y1) = p1.get()
+        (x2,y2) = p2.get()
+        dx = math.fabs(x1 - x2)
+        dy = math.fabs(y1 - y2)
+        euclidean = math.sqrt(dx*dx+dy*dy)
+        if (dx < self.tol and dx < dy/2):
+            mid_y = (y1+y2) / 2
+            mid_x = min(x1,x2) + euclidean
         else:
-            #more vertical, more arc
-            pass
+            mid_x = (x1 + x2) / 2
+            mid_y = min(y1,y2) + euclidean
+        #set the the step size
+        self.step = 1 / float(euclidean)
+        # add the three points
+        self.bezier.add_point(p1)
+        pmid = Point()
+        pmid.set(mid_x, mid_y)
+        self.bezier.add_point(pmid)
+        self.bezier.add_point(p2)
+        self.bezier.set_coeffs()
+        return 
 
-    def x_value(self,t):
-        return self.x0 + self.ux*t
+    def get(self):
+        '''
+        a function that returns the current points and moves to the next point
+        Parameters:
+            None
+        Returns:
+            p: the point (Point)
+        '''
+        p = self.bezier.get_point(self.u)
+        
+        self.u += self.step
+        if(self.u >= 1.0):
+            self.done = True
+        return p
     
-    def y_value(self,t):
-        return self.y0 + self.uy*t - 1/2*self.acceleration*t^2
+    def is_done(self):
+        '''
+        a function tot check if the equation has got to the second point
+        Parameters:
+            None
+        Returns:
+            done: True if equation is done False otherwise
+        '''
+        return self.done
 
-    def get_value(self,x):
-        #kinematic equation d=vi*t + 1/2*a*t^2
-        distance = self.velocity * x + 1/2*self.acceleration*x^2
-        return distance
+class TestEquation(unittest.TestCase):
+    def setUp(self):
+        self.e = Equation()
+
+    def tearDown(self):
+        pass
     
+    def test_simple_set_equation(self):
+        p = Point()
+        p.set(x=1,y=1)
+        p2 = Point()
+        p2.set(x=3,y=1)
+        self.e.set_equation(p, p2)
+        self.assertEqual(self.e.step, 0.5)
+        points = self.e.bezier.points
+        self.assertEqual(points[0].get(),(1,1))
+        self.assertEqual(points[1].get(),(2,3.0))
+        self.assertEqual(points[2].get(),(3,1))
+        self.assertEqual(self.e.get().get(), (1.0,1.0))
+        self.assertEqual(self.e.get().get(), (2.0,2.0))
+        self.assertEqual(self.e.get().get(), (3.0,1.0))
+        self.assertEqual(self.e.is_done(),True)
+    
+    def test_simple_set_equation(self):
+        p = Point()
+        p.set(x=1,y=1)
+        p2 = Point()
+        p2.set(x=3,y=1)
+        self.e.set_equation(p, p2)
+        self.assertEqual(self.e.step, 0.5)
+        points = self.e.bezier.points
+        self.assertEqual(points[0].get(),(1,1))
+        self.assertEqual(points[1].get(),(2,3.0))
+        self.assertEqual(points[2].get(),(3,1))
+        self.assertEqual(self.e.get().get(), (1.0,1.0))
+        self.assertEqual(self.e.get().get(), (2.0,2.0))
+        self.assertEqual(self.e.get().get(), (3.0,1.0))
+        self.assertEqual(self.e.is_done(),True) 
+
 class Cannonball():
     def __init__(self):
         '''
