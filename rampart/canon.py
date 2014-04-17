91f2ce05d18e46b488a60029cea0a13612cb2e6b
unknown <DFraser@GKTL0642.cds.int>
2014-04-17 10:57:59 -0400

    started working on cannon class

--- rampart/canon.py
+++ rampart/canon.py
@@ -5,3 +5,34 @@
 @date: 06/04/2014
 @note: This class is used for rampart game
 '''
+from cannonball import Cannonball
+from point import Point
+
+class Canon():
+    def __init__(self):
+        self.ball = Cannonball()
+        self.position = Point()
+    
+    def set(p0):
+        '''
+        a function that sets the position of the cannon
+        '''
+        self.position = p0
+    
+    def get(self):
+        '''
+        a function that sets the position of the cannon
+        Parameters:
+            None
+        Returns:
+            (x1,y1): the x and y position
+        '''
+    
+    def draw(self):
+        '''
+        a function to draw the cannon
+        Parameters:
+            None
+        Returns:
+            None
+        '''
