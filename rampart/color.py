5aa2bee9c69870fcd474fbd88b5e2c9a81e1f0f1
Dallas Fraser <fras2560@mylaurier.ca>
2014-04-28 12:24:58 -0400

    A class to store all game colors

---
+++ rampart/color.py
@@ -0,0 +1,22 @@
+'''
+@author: Dallas Fraser
+@contact: fras2560@mylaurier.ca
+@version: 1.0
+@date: 06/04/2014
+@note: This class is used for rampart game
+'''
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+GREEN    = (   0, 255,   0)
+RED      = ( 255,   0,   0)
+class Color():
+    def __init__(self):
+        self.purple =   ( 189,   0, 252)
+        self.green =    (   0, 255,   0)
+        self.red =      ( 255,   0,   0)
+        self.orange =   ( 252, 130,   0)
+        self.yellow =   ( 252, 252,   0)
+        self.blue =     (  13,   0, 252)
+        self.white =    ( 255, 255, 255)
+        self.black =    (   0,   0,   0)
+        self.grey =     ( 119, 119, 122)
