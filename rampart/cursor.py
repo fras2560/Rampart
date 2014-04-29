2f7575f9be55beb19256567762c1fed354add5e8
Dallas Fraser <fras2560@mylaurier.ca>
2014-04-29 17:17:14 -0400

    added comments for functions

--- rampart/cursor.py
+++ rampart/cursor.py
@@ -20,20 +20,54 @@ class Cursor(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
     
     def set(self, point):
+        '''
+        a function that sets the point of the cursor
+        Parameters:
+            point: the point of the cursor
+        Returns:
+            None
+        '''
         (x,y) = point.get()
         self.rect.x = x
         self.rect.y = y
     
     def get(self):
+        '''
+        a function to get the position of the cursor
+        Parameters:
+            None
+        Returns
+            (x,y): the x and y position (tuple)
+        '''
         return (self.rect.x,self.rect.y)
     
     def move(self,horizontal=0,vertical=0):
+        '''
+        a function to move the cursor
+        Parameters:
+            horizontal: the movement in the x axis
+            vertical: the movement in the y axis
+        Returns:
+            None
+        '''
         self.rect.x += horizontal
         self.rect.y += vertical
 
     def update(self):
+        '''
+        a function to update the cursor
+        Parameters:
+            None
+        Returns:
+            None
+        '''
         pass
 
     def draw(self,surface):
+        '''
+        a function to draw the cursor
+        Parameters:
+            surface: the surface to draw on
+        '''
         surface_blit = surface.blit
         surface_blit(self.image, self.rect)
