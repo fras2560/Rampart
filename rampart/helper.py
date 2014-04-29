541a9e691e09cfd1b5ef9f87954a6bdc1fb80818
Dallas Fraser <fras2560@mylaurier.ca>
2014-04-29 17:17:37 -0400

    added author and comments

--- rampart/helper.py
+++ rampart/helper.py
@@ -1,6 +1,21 @@
+'''
+@author: Dallas Fraser
+@contact: fras2560@mylaurier.ca
+@version: 1.0
+@date: 29/04/2014
+@note: random helper functions
+'''
 import os
 
+
 def file_path(name, image=None, sound=None):
+    '''
+    a function to helper find the filepath of the asset
+    Parameters:
+        name: the name of file
+        image: a boolean to specify if an image file or not
+        sound: a boolean to specify if an sound file or not
+    '''
     if image is not None:
         fullname = os.path.join("assets","images",name)
     if sound is not None:
