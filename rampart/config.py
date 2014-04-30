c3f0a0a94e8ac2a4e3042758a086c4f2f445a146
Dallas Fraser <fras2560@mylaurier.ca>
2014-04-30 15:52:39 -0400

    A file to hold game configurations and constants

---
+++ rampart/config.py
@@ -0,0 +1,34 @@
+'''
+@author: Dallas Fraser
+@contact: fras2560@mylaurier.ca
+@version: 1.0
+@date: 30/04/2014
+@note: A place to hold various constants and game configurations
+'''
+from math import pi
+'''
+directions"
+'''
+DOWN = 1
+UP = -1
+LEFT = -1
+RIGHT = 1
+'''
+game matrix constants
+'''
+EMPTY = 0
+BLOCK = 1
+CANNON = 2
+GRASS = 3
+WATER = 4
+PAINTED = 1
+'''
+movements
+'''
+NO_MOVE = 0
+CLOCKWISE = -pi/2
+COUNTER_CLOCKWISE = pi/2
+'''
+Physics constants
+'''
+GRAVITY = -0.049
