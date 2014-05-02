8163005fe3945e228522e08c4925f82795d26965
Dallas Fraser <fras2560@mylaurier.ca>
2014-05-02 17:21:33 -0400

    now able to update map with water or grass

--- rampart/TestMatrix.py
+++ rampart/TestMatrix.py
@@ -8,7 +8,7 @@
 from rampart import Rampart
 from piece import Piece
 import pygame
-from config import UP, DOWN, LEFT, RIGHT
+from config import UP, DOWN, LEFT, RIGHT, WATER, GRASS
 from color import Color
 SIZE = (500,500)
 class Tester():
@@ -17,7 +17,7 @@ class Tester():
         self.color = Color()
         pygame.init()
         self.screen = pygame.display.set_mode(SIZE)
-        pygame.display.set_caption("Test Matrix")
+        pygame.display.set_caption("Map Editor")
         pygame.key.set_repeat(1,10)
         self.header = pygame.font.SysFont("monospace", 36)
         self.point = pygame.font.SysFont('monospace', 18)
@@ -29,7 +29,7 @@ class Tester():
     
     def update_square(self, x,y):
         if self.selected > 0:
-            self.game.update_square()
+            self.rampart.game.update_square(x,y,self.selected)
 
     def main(self):
         self.rampart.game.load_level("test.txt")
@@ -37,6 +37,17 @@ class Tester():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.done = True
+                if event.type == pygame.KEYDOWN:
+                    key = pygame.key.get_pressed()
+                    if key[pygame.K_1]:
+                        self.selected = GRASS
+                    if key[pygame.K_2]:
+                        self.selected = WATER
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    x = pos[0]
+                    y = pos[1]
+                    self.update_square(x, y)
             self.screen.fill(self.color.white)
             self.rampart.draw(self.screen)
             pygame.display.flip()
