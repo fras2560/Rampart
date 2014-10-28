2430af3e263724e7621be62aac8c670edf3b3d4c
Dallas Fraser <fras2560@mylaurier.ca>
2014-10-27 23:01:57 -0400

    Just placeholders at this point

---
+++ game/rampart/level.py
@@ -0,0 +1,32 @@
+'''
+@author: Dallas Fraser
+@contact: fras2560@mylaurier.ca
+@version: 1.0
+@date: 10/27/2014
+@note: A level of rampart
+'''
+
+class Level():
+    def __init__(self):
+        pass
+
+import unittest
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
