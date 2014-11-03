572c1757d9d8f2c67fd1d74ae0c375f1264b83ea
fras2560 <fras2560@N2095-09-1.ad.wlu.ca>
2014-11-03 17:32:00 -0500

    Player now has a cursor object and a color

--- game/rampart/player.py
+++ game/rampart/player.py
@@ -7,15 +7,20 @@
 '''
 import helper
 import pygame
-from config import CASTLE, CANNON, LIVES
+from config import CASTLE, CANNON, LIVES, BUILDING, SHOOTING
 from rampart.cannonball import Cannonball
 from rampart.point import Point
+from rampart.cursor import Cursor
+from rampart.piece import Piece
+from rampart.color import Color
 class Player():
-    def __init__(self, id=None):
+    def __init__(self, id=None, point=None, color=None):
         '''
         this class is used for player interactions
         Parameters:
             id: the player id (int)
+            point: the starting point of the player's cursor
+            color: the player's color
         Properties:
             cannonballs: a dictionary to hold all the players cannonsballs and what
                         cannon shot the ball
@@ -32,6 +37,17 @@ class Player():
         if id is None:
             id = 1
         self.id = id
+        self.cursor = Cursor()
+        if point is None:
+            point = Point()
+            point.set(0, 0)
+        self.cursor.set(point)
+        self.piece = Piece()
+        self.piece.create_piece()
+        self.mode = BUILDING
+        if color is None:
+            color = Color().blue
+        self.color = color
 
     def reset(self):
         '''
@@ -68,38 +84,50 @@ class Player():
 
     def draw(self, surface):
         '''
-        a method to draw all the cannonballs of the player
+        a method to draw all the cannonballs of the player or the player's
+        piece depending on the mode
         Parameters:
             surface: the pygame display screen to draw on (surface)
         Returns:
             None
         '''
-        for ball in self.cannonballs:
-            self.cannonballs[ball].draw(surface)
+        if self.mode == SHOOTING:
+            for ball in self.cannonballs:
+                self.cannonballs[ball].draw(surface)
+            self.cursor.draw(surface)
+        else:
+            self.piece.draw(surface, self.color)
 
-    def shoot(self, x, y):
+    def shoot(self):
         '''
         a method for the player to shoot a cannonball at the specific position
         Parameters:
-            x: the x position to shoot for (int)
-            y: the y position to shoot for (int)
+            None
         Returns:
             shot: True if able to shoot cannonball False otherwise (boolean)
         '''
         shot = False
-        for cannon in self.guns:
-            position = cannon.get()
-            if position not in self.cannonballs:
-                ball = Cannonball()
-                p1 = Point()
-                p1.set(position[0], position[1])
-                p2 = Point()
-                p2.set(x, y)
-                ball.set(p1, p2)
-                self.cannonballs[position] = ball
-                shot = True
-                break
+        if self.mode == SHOOTING:
+            for cannon in self.guns:
+                position = cannon.get()
+                if position not in self.cannonballs:
+                    ball = Cannonball()
+                    p1 = Point()
+                    p1.set(position[0], position[1])
+                    p2 = Point()
+                    x, y = self.cursor.get()
+                    p2.set(x, y)
+                    ball.set(p1, p2)
+                    self.cannonballs[position] = ball
+                    shot = True
+                    break
         return shot
+
+    def move(self, horizontal=0, vertical=0):
+        if self.mode == SHOOTING:
+            self.cursor.move(horizontal, vertical)
+        elif self.mode == BUILDING:
+            self.piece.translate(horizontal, vertical)
 
     def update(self):
         ''''
@@ -113,7 +141,6 @@ class Player():
         nodes = []
         for ball in self.cannonballs:
             self.cannonballs[ball].update()
-            print(self.cannonballs[ball].get())
             if not self.cannonballs[ball].in_air():
                 delete.append(ball)
         for ball in delete:
@@ -130,6 +157,26 @@ class Player():
             self.id: the player id (int)
         '''
         return self.id
+
+    def shoot_mode(self):
+        '''
+        a method use to change the player's mode to shooing
+        Parameters:
+            None
+        Returns:
+            None
+        '''
+        self.mode = SHOOTING
+
+    def build_mode(self):
+        '''
+        a method use to change the player's mode to building
+        Parameters:
+            None
+        Returns:
+            None
+        '''
+        self.mode = BUILDING
 
 import unittest
 from rampart.color import Color
@@ -178,21 +225,31 @@ class PlayerTest(unittest.TestCase):
         self.addCannon()
         self.addCastle()
         self.player.draw(self.screen)
+        self.player.shoot_mode()
+        self.player.draw(self.screen)
 
     def testShoot(self):
         self.addCannon()
         self.addCannon(x=10, y=10)
-        shot1 = self.player.shoot(10, 10)
-        shot2 = self.player.shoot(100, 100)
-        shot3 = self.player.shoot(20, 20)
+        noshot = self.player.shoot()
+        self.assertEqual(noshot, False)
+        self.player.shoot_mode()
+        self.player.move(10, 0)
+        shot1 = self.player.shoot()
+        self.player.move(90, 90)
+        shot2 = self.player.shoot()
+        self.player.move(-80, -80)
+        shot3 = self.player.shoot()
         self.assertEqual(shot1, True)
         self.assertEqual(shot2, True)
         self.assertEqual(shot3, False)
 
     def testUpdate(self):
+        self.player.shoot_mode()
         self.addCannon()
         self.addCannon(x=10, y=10)
-        shot1 = self.player.shoot(10, 10)
+        self.player.move(10, 10)
+        shot1 = self.player.shoot()
         delete = self.player.update()
         while delete == []:
             self.player.draw(self.screen)
