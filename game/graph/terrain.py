7c54a18883e2463c80248584c25d4c966aef162e
fras2560 <dallas_j_frase@hotmail.com>
2014-11-08 12:20:53 -0500

    an object to hold all the terrain images.

---
+++ game/graph/terrain.py
@@ -0,0 +1,76 @@
+'''
+@author: Dallas Fraser
+@contact: fras2560@mylaurier.ca
+@version: 1.0
+@date: 11/08/2014
+@note: Holds all the terrain images
+'''
+import pygame
+from rampart.helper import file_path
+class Terrain():
+    '''
+    a class to hold all the terrain images and rects
+    '''
+    def __init__(self, terrain_to_files, terrain, color):
+        '''
+        Terrain()
+        Parameters:
+            terrain_to_files: a dictionary of the terrains, their states and 
+                             images for each terrain state (dict)
+            terrain: the size of the terrain (int)
+            color: the color of the background (RR,GG,BB)
+        '''
+        self.images ={}
+        self.rects = {}
+        for terrain in terrain_to_files:
+            self.images[terrain] = []
+            self.rects[terrain] = []
+            for state in terrain_to_files[terrain]:
+                for pic in state:
+                    images = []
+                    rects = []
+                    fp = file_path(pic, image=True)
+                    image = pygame.image.load(fp).convert()
+                    image = pygame.transform.scale(image, (terrain, terrain))
+                    image.set_colorkey(color)
+                    rect = image.get_rect()
+                    images.append(image)
+                    rects.append(rect)
+                self.images[terrain].append(images)
+                self.rects[terrain].append(rects)
+
+    def get_image(self, terrain, state):
+        return self.images[terrain][state]
+
+    def get_rect(self, terrain, state):
+        return self.rects[terrain][state]
+
+
+import unittest
+from rampart.config import TERRAIN_TO_FILE, NODE_SIZE, SIZE, TYPES
+from rampart.config import NORMAL, DESTROYED, UNPAINTED
+
+class TerrainTeser(unittest.TestCase):
+    def setUp(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(SIZE)
+        pygame.display.set_caption("Test Node Object")
+        self.terrain = Terrain(TERRAIN_TO_FILE, NODE_SIZE, ( 189,   0, 252))
+
+    def tearDown(self):
+        pass
+
+    def testGetImage(self):
+        states = [NORMAL, DESTROYED, UNPAINTED]
+        for t in TYPES:
+            for s in states:
+                self.terrain.get_image(t, s)
+
+    def testGetRect(self):
+        states = [NORMAL, DESTROYED, UNPAINTED]
+        for t in TYPES:
+            for s in states:
+                self.terrain.get_rect(t, s)
+
+if __name__ == "__main__":
+    unittest.main()
